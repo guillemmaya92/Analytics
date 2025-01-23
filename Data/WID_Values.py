82eb45266ebe5bb85630dd8a28a987bbc6cf090c
guillemmaya92 <guillem.maya92@outlook.com>
2025-01-23 20:24:07 +0100

    add WID_Values numbers

--- Data/WID_Values.py
+++ Data/WID_Values.py
@@ -35,19 +35,23 @@ dfx = pd.concat(list, ignore_index=True)
 # Data Manipulation
 # ===================================================
 # Filter dataframe
-variable = ['gdiincj992', 'ghwealj992', 'adiincj992', 'ahweali992', 'npopuli999', 'xlceuxi999', 'xlcusxi999']
+variable = ['mgdproi999', 'gdiincj992', 'ghwealj992', 'adiincj992', 'anninci992', 'ahweali992', 'anweali992', 'npopuli999', 'xlceuxi999', 'xlcusxi999', 'wwealni999']
 percentile = ['p0p100']
 df = dfx[dfx['variable'].isin(variable) & dfx['percentile'].isin(percentile)].copy()
 
 # Rename variable
 df['variable'] = df['variable'].replace({
+    'mgdproi999': 'gdptotal',
     'gdiincj992': 'gincome', 
     'ghwealj992': 'gwealth', 
     'adiincj992': 'tincome',
+    'anninci992': 'tincome2',
     'ahweali992': 'twealth',
+    'anweali992': 'twealth2',
     'npopuli999': 'population',
     'xlceuxi999': 'xeur',
-    'xlcusxi999': 'xusd'}) 
+    'xlcusxi999': 'xusd',
+    'wwealni999': 'wiratio'}) 
 df = df[['variable', 'country', 'year', 'value']]
 
 # Unpivot to columns
@@ -55,7 +59,9 @@ df = df.pivot_table(index=['country', 'year'], columns='variable', values='value
 df = df.reset_index()
 
 # Select and order columns
-df = df[['country', 'year', 'tincome', 'twealth', 'gincome', 'gwealth', 'population', 'xeur', 'xusd']]
+df = df[['country', 'year', 'gdptotal', 'tincome', 'tincome2', 'twealth', 'twealth2', 'gincome', 'gwealth', 'population', 'xeur', 'xusd', 'wiratio']]
 
 # Save to parquet
 df.to_parquet(r'C:\Users\guill\Downloads\WID_Values.parquet', engine='pyarrow')
+
+print(df)
